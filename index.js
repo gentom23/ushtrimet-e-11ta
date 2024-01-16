e146c7036cad0a5a76e269bc05ec40dd05b5107e
gentom231 <gzarari@gmail.com>
2024-01-16 20:54:14 +0100

    shkrova titullin dhe paragrafin

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+let page = {
+    permbajtjaETitullit: "genti",
+    permbajtjaEFaqes: "blablabla"
+}
+
+
+let Titulli = document.getElementById("Titulli")
+Titulli.innerHTML = page.permbajtjaETitullit
+
+let faqja = document.getElementById("faqja")
+faqja.innerHTML = page.permbajtjaEFaqes
+
+
+
+
+
+
